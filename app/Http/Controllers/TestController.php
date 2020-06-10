51e7471d57778c405029290a33e81979d1fda0ea
Artem Melnik <artemmelnik989@gmail.com>
2020-06-10 20:20:08 +0300

    fix

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -48,7 +48,7 @@ class TestController extends Controller
         $correctly      = $request->get('correctly');
 
         $dateStart = new \DateTime($testResult['created_at']);
-        $dateEnd   = (new \DateTime($testResult['created_at']))->modify('+60 minutes');
+        $dateEnd   = (new \DateTime($testResult['created_at']))->modify('+59 minutes');
         $dateNow   = new \DateTime;
         $dateDiff  = $dateStart->diff($dateNow);
 
